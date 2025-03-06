6d07dea382bd678941b1596c46bfe55ff875b3b6
Wergens <you@example.com>
2025-03-06 13:33:48 +0000

    standardize sample_llm_api.py 	create list[LLM] and standardize sample_llm_api_test.py

--- sample_llm_api.py
+++ sample_llm_api.py
@@ -1,25 +1,25 @@
 import dataclasses
 import numpy as np
 '''
-LLM_Name: 大模型的名字
-API_Key: 调用大模型的密钥
+llm_name: 大模型的名字
+api_key: 调用大模型的密钥
 Model: 大模型的型号
-Provider: 大模型的提供商
-Input_price: 输入的价格,RMB/million tokens
-Output_price: 输出的价格,RMB/million tokens
-Response_score: 记录大模型回复的得分
-Score: 大模型的评分
+provider: 大模型的提供商
+input_price: 输入的价格,RMB/million tokens
+output_price: 输出的价格,RMB/million tokens
+response_score: 记录大模型回复的得分
+score: 大模型的评分
 '''
 @dataclasses.dataclass
 class LLM:
-    LLM_Name: str
-    API_Key: str
-    Model: str
-    Provider: str
-    Input_price: float
-    Output_price: float
-    Response_score: list = dataclasses.field(default_factory=list)
-    Score: float = 0
+    llm_name: str
+    api_key: str
+    model: str
+    provider: str
+    input_price: float
+    output_price: float
+    response_score: list = dataclasses.field(default_factory=list)
+    score: float = 0
 
 '''
 llm_list: 存储所有的大模型的list[LLM]
@@ -27,7 +27,7 @@ basescore: 基准评分
 self._Max_score: 最高得分,初始为负无穷
 Base_price_score: 价格评分的基准值
 '''
-class Evaluate_LLM:
+class EvaluateLLM:
 
     def __init__(self, llm_list: list[LLM], basescore: float, Base_price_score: float):
         self._llm_list = llm_list
@@ -35,40 +35,40 @@ class Evaluate_LLM:
         self._basescore = basescore
         self._Base_price_score = Base_price_score
         # 根据价格初始化初始评分
-        self._total_cost = np.sum([llm.Input_price + llm.Output_price for llm in self._llm_list])
+        self._total_cost = np.sum([llm.input_price + llm.output_price for llm in self._llm_list])
         for index in range(len(self._llm_list)):
             # 计算价格权重占比
-            cost_ratio = (self._llm_list[index].Input_price + self._llm_list[index].Output_price) / self._total_cost
-            self._llm_list[index].Score = self._Base_price_score * (1 - cost_ratio)
+            cost_ratio = (self._llm_list[index].input_price + self._llm_list[index].output_price) / self._total_cost
+            self._llm_list[index].score = self._Base_price_score * (1 - cost_ratio)
 
     # 将大模型注册到评分器中
     def _register_llm(self, llm: LLM):
         self._llm_list.append(llm)
         index = self._llm_list.index(llm)
         # 根据价格初始化初始评分
-        self._total_cost = self._total_cost + llm.Input_price + llm.Output_price
+        self._total_cost = self._total_cost + llm.input_price + llm.output_price
         # 计算价格权重占比
-        cost_ratio = (llm.Input_price + llm.Output_price) / self._total_cost
-        self._llm_list[index].Score = self._Base_price_score * (1 - cost_ratio)
+        cost_ratio = (llm.input_price + llm.output_price) / self._total_cost
+        self._llm_list[index].score = self._Base_price_score * (1 - cost_ratio)
     
     # 每次调用大模型后,将得分记录到大模型的Response_score中
     def call_llm(self, llm: LLM, score: float):
         index = self._llm_list.index(llm)
         if(self._Max_score == float("-inf")):
             self._Max_score = score
-        self._llm_list[index].Response_score.append((score-self._basescore, score-self._Max_score))
+        self._llm_list[index].response_score.append((score-self._basescore, score-self._Max_score))
         self._Max_score = max(self._Max_score, score)
 
         decay_factor = 0.8 # 历史衰减系数
-        if len(llm.Response_score) == 1:
+        if len(llm.response_score) == 1:
             '''
             该模型只被调用过一次
             score1: 根据该次调用的得分与basescore的差值对模型进行评分
             score2: 根据该次调用的得分与Max_score的差值对模型进行评分
             '''
-            score1 = self._llm_list[index].Response_score[0][0]
-            score2 = self._llm_list[index].Response_score[0][1]
-            delta = (score1 + score2) * (1 - decay_factor)
+            score1 = self._llm_list[index].response_score[0][0]
+            score2 = self._llm_list[index].response_score[0][1]
+            delta = (score1 + score2)
         else:
             '''
             该模型被调用过多次
@@ -76,25 +76,27 @@ class Evaluate_LLM:
             score2: 根据该次调用的得分与Max_score的差值对模型进行评分
             score3: 根据该次调用的得分与上一次调用的得分的差值对模型进行评分
             '''
-            score1 = self._llm_list[index].Response_score[-1][0]
-            score2 = self._llm_list[index].Response_score[-1][1]
-            score3 = self._llm_list[index].Response_score[-1][0] - self._llm_list[index].Response_score[-2][0]
+            score1 = self._llm_list[index].response_score[-1][0]
+            score2 = self._llm_list[index].response_score[-1][1]
+            score3 = self._llm_list[index].response_score[-1][0] - self._llm_list[index].response_score[-2][0]
             # 模型调用次数越多,当前次数的权重越大
-            delta = (score1 + score2 + score3) * (1 - decay_factor ** len(llm.Response_score))
+            delta = (score1 + score2 + score3) * (1 - decay_factor ** len(llm.response_score))
         # 更新模型的评分(平滑过渡)
-        self._llm_list[index].Score = decay_factor * self._llm_list[index].Score + delta
+        self._llm_list[index].score = decay_factor * self._llm_list[index].score + (1 - decay_factor) * delta
 
     # 计算下次选择大模型时使用的评分概率
     def calculate_probability(self):
-        score_list = np.array([llm.Score for llm in self._llm_list])
+        score_list = np.array([llm.score for llm in self._llm_list])
         # 标准化得分
         scores = (score_list - np.min(score_list)) / (np.max(score_list) - np.min(score_list) + 1e-6)
         # 动态温度调整（鼓励探索）
-        temperature = max(0.5, 1.0 - 0.1 * len(self._llm_list[0].Response_score))
+        min_temperature = 0.5 # 最小温度
+        temperature = max(min_temperature, 1.0 - 0.1 * len(self._llm_list[0].response_score))
         # 带平滑的softmax（确保最小概率）
         exp_scores = np.exp(scores / temperature)
         probabilities = exp_scores / (exp_scores.sum() + 1e-6)
-        probabilities = 0.8 * probabilities + 0.2 * (1/len(probabilities))  # 混合均匀分布
+        ratio = 0.9 # 混合均匀分布的比例
+        probabilities = ratio * probabilities + (1 - ratio) * (1/len(probabilities))  # 混合均匀分布
         return probabilities
 
 
--- sample_llm_api_test.py
+++ sample_llm_api_test.py
@@ -1,31 +1,166 @@
 from sample_llm_api import LLM
-from sample_llm_api import Evaluate_LLM
+from sample_llm_api import EvaluateLLM
 
-DeepSeek_R1 = LLM("DeepSeek-R1", "Bearer f184bcd9-68b0-49be-8a3f-ea095ee71e14", "ep-20250303202036-j6hfh", "", 4.0, 16.0, [])
-DeepSeek_V3 = LLM("DeepSeek-V3", "Bearer f184bcd9-68b0-49be-8a3f-ea095ee71e14", "ep-20250227102412-tfkv8", "", 2.0, 8.0, [])
-Deepseek_R1_distill_qwen_7b = LLM("Deepseek-R1-distill-qwen-7b", "Bearer f184bcd9-68b0-49be-8a3f-ea095ee71e14", "ep-20250305162451-qxgns", "", 0.6, 2.4, [])
-DeepSeek_R1_distill_qwen_32b = LLM("DeepSeek-R1-distill-qwen-32b", "Bearer f184bcd9-68b0-49be-8a3f-ea095ee71e14", "ep-20250305162537-2sbpv", "", 1.5, 6.0, [])
+ds_byte_key = "Bearer f184bcd9-68b0-49be-8a3f-ea095ee71e14"
+gpt_byte_key = "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"
+claude_key = None
+gemini_key = None
+grok_key = None
+rate = 7.2739
 
-GPT_4o = LLM("GPT-4o", "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", "", "OpenAI", 18.18475, 72.739, [])
-GPT_4o_mini = LLM("GPT-4o-mini", "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", "", "OpenAI", 1.091085, 4.36434, [])
-GPT_o1 = LLM("GPT-o1", "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", "", "OpenAI", 109.1085, 436.434, [])
-GPT_o1_mini = LLM("GPT-o1-mini", "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", "", "OpenAI", 8.00129, 32.00516, [])
-GPT_o3_mini = LLM("GPT-o3-mini", "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", "", "OpenAI", 8.00129, 32.00516, [])
-GPT_o3_mini_high = LLM("GPT-o3-mini-high", "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", "", "OpenAI", 8.00129, 32.00516, [])
-
-Claude_3_7_sonnet = LLM("Claude-3.7-sonnet", "", "", "Anthropic", 21.8217, 109.1085, [])
-
-Gemini_2_0_flash_001 = LLM("Gemini-2.0-flash-001", "", "", "Google AI Studio", 0.72739, 2.90956, [])
-Gemini_2_0_pro_exp_02_05 = LLM("Gemini-2.0-pro-exp-02-05", "", "", "Google AI Studio", 0.0, 0.0, [])
-Gemini_2_0_flash_thinking_exp = LLM("Gemini-2.0-flash-thinking-exp", "", "", "Google AI Studio", 0.0, 0.0, [])
-
-Grok_beta = LLM("Grok-beta", "", "", "xAI", 36.3695, 109.1085, [])
-
-llm_list = [DeepSeek_R1, DeepSeek_V3, Deepseek_R1_distill_qwen_7b, DeepSeek_R1_distill_qwen_32b, GPT_4o, GPT_4o_mini, GPT_o1, GPT_o1_mini, GPT_o3_mini, GPT_o3_mini_high, Claude_3_7_sonnet, Gemini_2_0_flash_001, Gemini_2_0_pro_exp_02_05, Gemini_2_0_flash_thinking_exp, Grok_beta]
-
-llm = Evaluate_LLM(llm_list, 0.0, 10.0)
-llm.call_llm(DeepSeek_R1, 1.0)
+llm_list = [
+    LLM(
+        llm_name="DeepSeek-R1",
+        api_key=ds_byte_key,
+        model="ep-20250303202036-j6hfh",
+        provider=None,
+        input_price=4.0,
+        output_price=16.0,
+        response_score=[],
+        score=0.0,
+    ),
+    LLM(
+        llm_name="DeepSeek-V3", 
+        api_key=ds_byte_key, 
+        model="ep-20250227102412-tfkv8", 
+        provider=None, 
+        input_price=2.0,
+        output_price=8.0, 
+        response_score=[], 
+        score=0.0
+    ),
+    LLM(
+        llm_name="Deepseek-R1-distill-qwen-7b",
+        api_key=ds_byte_key,
+        model="ep-20250305162451-qxgns",
+        provider=None,
+        input_price=0.6,
+        output_price=2.4,
+        response_score=[],
+        score=0.0,
+    ),
+    LLM(
+        llm_name="DeepSeek-R1-distill-qwen-32b",
+        api_key=ds_byte_key,
+        model="ep-20250305162537-2sbpv",
+        provider=None,
+        input_price=1.5,
+        output_price=6.0,
+        response_score=[],
+        score=0.0,
+    ),
+    LLM(
+        llm_name="GPT-4o",
+        api_key=gpt_byte_key,
+        model=None,
+        provider="OpenAI",
+        input_price=2.5 * rate,
+        output_price=10 * rate,
+        response_score=[],
+        score=0.0,
+    ),
+    LLM(
+        llm_name="GPT-4o-mini",
+        api_key=gpt_byte_key,
+        model=None,
+        provider="OpenAI",
+        input_price=0.15 * rate,
+        output_price=0.6 * rate,
+        response_score=[],
+        score=0.0,
+    ),
+    LLM(
+        llm_name="GPT-o1",
+        api_key=gpt_byte_key,
+        model=None,
+        provider="OpenAI",
+        input_price=15 * rate,
+        output_price=60 * rate,
+        response_score=[],
+        score=0.0,
+    ),
+    LLM(
+        llm_name="GPT-o1-mini",
+        api_key=gpt_byte_key,
+        model=None,
+        provider="OpenAI",
+        input_price=1.1 * rate,
+        output_price=4.4 * rate,
+        response_score=[],
+        score=0.0,
+    ),
+    LLM(
+        llm_name="GPT-o3-mini",
+        api_key=gpt_byte_key,
+        model=None,
+        provider="OpenAI",
+        input_price=1.1 * rate,
+        output_price=4.4 * rate,
+        response_score=[],
+        score=0.0,
+    ),
+    LLM(
+        llm_name="GPT-o3-mini-high",
+        api_key=gpt_byte_key,
+        model=None,
+        provider="OpenAI",
+        input_price=1.1 * rate,
+        output_price=4.4 * rate,
+        response_score=[],
+        score=0.0,
+    ),
+    LLM(
+        llm_name="Claude-3.7-sonnet",
+        api_key=claude_key,
+        model=None,
+        provider="Anthropic",
+        input_price=3 * rate,
+        output_price=15 * rate,
+        response_score=[],
+        score=0.0,
+    ),
+    LLM(
+        llm_name="Gemini-2.0-flash-001",
+        api_key=gemini_key,
+        model=None,
+        provider="Google AI Studio",
+        input_price=0.1 * rate,
+        output_price=0.4 * rate,
+        response_score=[],
+        score=0.0,
+    ),
+    LLM(
+        llm_name="Gemini-2.0-pro-exp-02-05",
+        api_key=gemini_key,
+        model=None,
+        provider="Google AI Studio",
+        input_price=0.0 * rate,
+        output_price=0.0 * rate,
+        response_score=[],
+        score=0.0,
+    ),
+    LLM(
+        llm_name="Gemini-2.0-flash-thinking-exp",
+        api_key=gemini_key,
+        model=None,
+        provider="Google AI Studio",
+        input_price=0.0 * rate,
+        output_price=0.0 * rate,
+        response_score=[],
+        score=0.0,
+    ),
+    LLM(
+        llm_name="Grok-beta",
+        api_key=grok_key,
+        model=None,
+        provider="xAI",
+        input_price=5 * rate,
+        output_price=15 * rate,
+        response_score=[],
+        score=0.0,
+    ),
+]
+llm = EvaluateLLM(llm_list, 0.0, 5.0)
+llm.call_llm(llm_list[0], 10.0)
 p = llm.calculate_probability()
 print(p)
-
-    
